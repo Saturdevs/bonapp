0f3db0e46a0f16c5572e6bf93ca3698d8a195650
Lorenzo Lischetti <lischetti.lorenzo@gmail.com>
2019-06-30 23:12:40 -0300

    Added validation to validate if the payment type has any cash movements when the user wants to delete it

--- controllers/paymentType.js
+++ controllers/paymentType.js
@@ -1,6 +1,10 @@
 'use strict'
 
 const PaymentType = require('../models/paymentType')
+const Client = require('../models/client')
+const CashFlow = require('../models/cashFlow')
+const Order = require('../models/order')
+const CashCount = require('../models/arqueoCaja')
 
 function getPaymentTypes (req, res) {
   PaymentType.find({}, null, {sort: {name: 1}}, (err, paymentTypes) => {
@@ -74,16 +78,32 @@ function updatePaymentType (req, res) {
   })
 }
 
-function deletePaymentType (req, res) {
+async function deletePaymentType (req, res) {
   let paymentTypeId = req.params.paymentTypeId
 
-  PaymentType.findById(paymentTypeId, (err, paymentType) => {
+  await PaymentType.findById(paymentTypeId, async (err, paymentType) => {
     if (err) return res.status(500).send({ message: `Error al querer borrar el medio de pago: ${err}`})
 
-    paymentType.remove(err => {
-      if (err) return res.status(500).send({ message: `Error al querer borrar el medio de pago: ${err}`})
-      res.status(200).send({message: `El medio de pago ha sido eliminado`})
-    })
+    if (paymentType !== 'undefined' && paymentType !== null) {
+      let validationMessages = [];
+      let validationError;
+      validationMessages = await validateDelete(paymentTypeId)
+      if (validationMessages.length === 0) {
+        paymentType.remove(err => {
+          if (err) return res.status(500).send({ message: `Error al querer borrar el medio de pago: ${err}`})
+          res.status(200).send({message: `El medio de pago ha sido eliminado`})
+        })
+      } else {
+        validationError = `Este medio de pago (${paymentType.name}) no puede ser eliminado debido a que:`
+
+        validationMessages.forEach(message => {
+          validationError += '\n\r\t- ' + message;            
+        });
+
+        validationError += '\n\rMárquelo como inactivo en su lugar.'
+        return res.status(500).send({ message: validationError})        
+      } 
+    }    
   })
 }
 
@@ -93,6 +113,65 @@ function unSetDefaultPaymentType (req, res) {
     if (err) return handleError(err);
     res.status(200).send({message: `El tipo de pago por defecto ha sido cambiado`})
   });
+}
+
+async function validateDelete(paymentMethodId) {
+
+  let validationErrors = [];
+  
+  await CashFlow.find({paymentType: paymentMethodId}, (err, cashFlows) => {
+    if (err) return res.status(500).send({ message: `Hubo un error al querer eliminar el tipo de pago (falló ka validación de movimientos de caja) ${err}`})
+
+    if (cashFlows !== null && cashFlows !== 'undefined' && cashFlows.length > 0) {
+      validationErrors.push('Tiene MOVIMIENTOS DE CAJA asociados')
+    }
+  })
+  
+  await Client.find({}, (err, clients) => {
+    if (err) return res.status(500).send({ message: `Hubo un error al querer eliminar el tipo de pago (falló la validación de transacciones de clientes) ${err}`})
+
+    let foundClient = false;
+    for (let i = 0; i < clients.length && !foundClient; i++) {
+      for (let j = 0; j < clients[i].transactions.length && !foundClient; j++) {                
+        if (clients[i].transactions[j].paymentMethod.toString() === paymentMethodId) {          
+          validationErrors.push('Tiene TRANSACCIONES asociadas')
+          foundClient = true;
+        }
+      }
+    }
+  })  
+
+  await Order.find({}, (err, orders) => {
+    if (err) return res.status(500).send({ message: `Hubo un error al querer eliminar el tipo de pago (falló la validación de pedidos) ${err}`})
+
+    let foundOrder = false;
+    for (let i = 0; i < orders.length && !foundOrder; i++) {
+      for (let j = 0; j < orders[i].users.length && !foundOrder; j++) {
+        for (let k = 0; k < orders[i].users[j].payments.length && !foundOrder; k++) {
+          if (orders[i].users[j].payments[k].methodId.toString() === paymentMethodId) {
+            validationErrors.push('Tiene PEDIDOS asociados')
+            foundOrder = true;
+          }
+        }
+      }
+    }    
+  })
+
+  await CashCount.find({}, (err, cashCounts) => {
+    if (err) return res.status(500).send({ message: `Hubo un error al querer eliminar el tipo de pago (falló la validación de arqueos) ${err}`})
+
+    let foundCashCount = false;
+    for (let i = 0; i < cashCounts.length && !foundCashCount; i++) {
+      for (let j = 0; j < cashCounts[i].realAmount.length && !foundCashCount; j++) {
+        if (cashCounts[i].realAmount[j].paymentType.toString() === paymentMethodId) {
+          validationErrors.push('Tiene ARQUEOS asociados')
+          foundCashCount = true;
+        }
+      }
+    }    
+  })
+
+  return validationErrors
 }
 
 module.exports = {
