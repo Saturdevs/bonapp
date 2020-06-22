c4e9c6fa30f2a41b76eac2924b01d60884b6cba6
imchiodo <imchiodo1@gmail.com>
2020-06-22 09:05:44 -0300

    subscriptionModel

--- models/subscription.js
+++ models/subscription.js
@@ -4,8 +4,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const subscriptionSchema = Schema({
-  //Este modelo está vacío porque se guardan las subscripciones que se recibien de la api de google cuando
-  //el usuario acepta recibir notificaciones. NO tenemos un modelo propio.
+  endpoint: { type: String, required: true },
+  expirationTime: { type: Date },
+  options: { 
+    applicationServerKey: { type: ArrayBuffer },
+    userVisibleOnly: { type: Boolean },
+   }
 });
 
 module.exports = mongoose.model('Subscription', subscriptionSchema);
