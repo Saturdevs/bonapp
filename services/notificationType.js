b80b89c30b9db04dad3b4d3a128dee872c1f7828
imchiodo <imchiodo1@gmail.com>
2020-06-23 23:09:06 -0300

    notificationType Service

--- services/notificationType.js
+++ services/notificationType.js
@@ -1,5 +1,5 @@
 'use strict';
-const NotificationTypeDAO = require('');
+const NotificationTypeDAO = require('../dataAccess/notificationType');
 
 async function getNotificationType(notificatioTypeId) {
     try {
