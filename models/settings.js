9ec43128822da34d9a4708e576322a59bbe9b7fa
Lorenzo Lischetti <lischetti.lorenzo@gmail.com>
2021-04-24 14:21:47 -0300

    Added business type (hotel, bar)

--- models/settings.js
+++ models/settings.js
@@ -2,15 +2,16 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const BUSINESS_TYPES = require('../shared/enums/businessTypes');
 
 const settingsSchema = Schema({
-  _id: { type: String, required: true },
-  restaurantId: { type: String, required: true }, 
+  restaurantId: { type: String, required: true, unique: true }, 
   name: {type: String, required: true},
   phoneNumber: { type: String },
   email: { type: String }, 
   address: { type: String }, 
-  wifi: { type: String }
+  wifi: { type: String },
+  businessType: { type: String, required: true, enum: [BUSINESS_TYPES.BAR, BUSINESS_TYPES.HOTEL] }
 });
 
 module.exports = mongoose.model('Settings', settingsSchema);
---
+++ shared/enums/businessTypes.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = {
+  BAR: 'BAR',
+  HOTEL: 'HOTEL'
+}
